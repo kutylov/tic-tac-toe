30963d39af24858a4034a853ba9d2d0a2cfa1289
dolf <kolyakutylov@gmail.com>
2022-05-10 23:45:58 +0700

    Add Game class with child components

--- src/academy/devonline/tictactoe/WinnerVerifier.java
+++ src/academy/devonline/tictactoe/WinnerVerifier.java
@@ -20,15 +20,13 @@ package academy.devonline.tictactoe;
  * @author devonline
  * @link http://devonline.academy/java
  */
-public final class Launcher {
-    public static void main(final String[] args) {
-        final Game game = new Game(
-                new DataPrinter(),
-                new ComputerMove(),
-                new UserMove(),
-                new WinnerVerifier(),
-                new DrawVerifier()
-        );
-        game.play();
+public class WinnerVerifier {
+
+    public boolean isUserWin(final GameTable gameTable) {
+        return false;
+    }
+
+    public boolean isComputerWin(final GameTable gameTable) {
+        return false;
     }
 }
